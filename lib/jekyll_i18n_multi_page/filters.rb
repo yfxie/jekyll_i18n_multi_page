2885836c67fdce1e6eaa065475798a92e10aff32
Yi Feng <yfxie@me.com>
2019-10-29 18:19:26 +0800

    fixed: nested attributes can be accessed

--- lib/jekyll_i18n_multi_page/filters.rb
+++ lib/jekyll_i18n_multi_page/filters.rb
@@ -1,7 +1,8 @@
 module JekyllI18nMultiPage
   module Filters
     def i18n(path)
-      I18n.t(path, locale: @context['page']['i18n_locale'])
+      result = I18n.t(path, locale: @context['page']['i18n_locale'])
+      { result: result }.with_indifferent_access[:result]
     end
   end
 end
